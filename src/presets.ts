9f6bd8f8dea70ba6c8e5cfe8941d45a03751fed6
Avirup Ghosh <oviirup@gmail.com>
2025-10-08 23:38:58 +0530

    🔧 updated presets

--- src/presets.ts
+++ src/presets.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { envalid } from "./index";
 
 /**
- * MARK: Vercel ENV
+ * Vercel System Environment Variables
  *
  * @see https://vercel.com/docs/projects/environment-variables/system-environment-variables#system-environment-variables
  */
@@ -10,11 +10,14 @@ export const vercel = () =>
   envalid({
     server: {
       VERCEL: z.string().optional(),
+      CI: z.string().optional(),
       VERCEL_ENV: z.enum(["development", "preview", "production"]).optional(),
       VERCEL_URL: z.string().optional(),
       VERCEL_PROJECT_PRODUCTION_URL: z.string().optional(),
       VERCEL_BRANCH_URL: z.string().optional(),
       VERCEL_REGION: z.string().optional(),
+      VERCEL_DEPLOYMENT_ID: z.string().optional(),
+      VERCEL_SKEW_PROTECTION_ENABLED: z.string().optional(),
       VERCEL_AUTOMATION_BYPASS_SECRET: z.string().optional(),
       VERCEL_GIT_PROVIDER: z.string().optional(),
       VERCEL_GIT_REPO_SLUG: z.string().optional(),
@@ -28,26 +31,6 @@ export const vercel = () =>
       VERCEL_GIT_PREVIOUS_SHA: z.string().optional(),
       VERCEL_GIT_PULL_REQUEST_ID: z.string().optional(),
     },
-
-    // prettier-ignore
-    runtimeEnv: {
-      VERCEL: process.env.VERCEL,
-      VERCEL_ENV: process.env.VERCEL_ENV,
-      VERCEL_URL: process.env.VERCEL_URL,
-      VERCEL_PROJECT_PRODUCTION_URL: process.env.VERCEL_PROJECT_PRODUCTION_URL,
-      VERCEL_BRANCH_URL: process.env.VERCEL_BRANCH_URL,
-      VERCEL_REGION: process.env.VERCEL_REGION,
-      VERCEL_AUTOMATION_BYPASS_SECRET: process.env.VERCEL_AUTOMATION_BYPASS_SECRET,
-      VERCEL_GIT_PROVIDER: process.env.VERCEL_GIT_PROVIDER,
-      VERCEL_GIT_REPO_SLUG: process.env.VERCEL_GIT_REPO_SLUG,
-      VERCEL_GIT_REPO_OWNER: process.env.VERCEL_GIT_REPO_OWNER,
-      VERCEL_GIT_REPO_ID: process.env.VERCEL_GIT_REPO_ID,
-      VERCEL_GIT_COMMIT_REF: process.env.VERCEL_GIT_COMMIT_REF,
-      VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA,
-      VERCEL_GIT_COMMIT_MESSAGE: process.env.VERCEL_GIT_COMMIT_MESSAGE,
-      VERCEL_GIT_COMMIT_AUTHOR_LOGIN: process.env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN,
-      VERCEL_GIT_COMMIT_AUTHOR_NAME: process.env.VERCEL_GIT_COMMIT_AUTHOR_NAME,
-      VERCEL_GIT_PREVIOUS_SHA: process.env.VERCEL_GIT_PREVIOUS_SHA,
-      VERCEL_GIT_PULL_REQUEST_ID: process.env.VERCEL_GIT_PULL_REQUEST_ID,
-    },
+    // @ts-expect-error unnecessarily cautious
+    runtimeEnv: process.env,
   });
